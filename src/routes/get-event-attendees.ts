44ae1e735955f7cdbb9e82d5154017231a659dce
ThiagoMoraes21 <thiago4x70@gmail.com>
2024-04-07 16:16:35 -0300

    feat: add route for getting the event attendees

---
+++ src/routes/get-event-attendees.ts
@@ -0,0 +1,91 @@
+import { FastifyInstance } from "fastify";
+import { ZodTypeProvider } from "fastify-type-provider-zod";
+import { z } from "zod";
+import { prisma } from "../lib/prisma";
+
+export async function getEventAttendees(app: FastifyInstance) {
+  app.withTypeProvider<ZodTypeProvider>().get(
+    "/events/:eventId/attendees",
+    {
+      schema: {
+        summary: "Get event attendees",
+        tags: ["events"],
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        querystring: z.object({
+          query: z.string().nullish(),
+          pageIndex: z.string().nullish().default("0").transform(Number),
+        }),
+        response: {
+          200: z.object({
+            attendees: z.array(
+              z.object({
+                id: z.number(),
+                name: z.string(),
+                email: z.string().email(),
+                createdAt: z.date(),
+                checkedInAt: z.date().nullable(),
+              }),
+            ),
+            total: z.number(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params;
+      const { pageIndex, query } = request.query;
+
+      const attendeesPromise = prisma.attendee.findMany({
+        select: {
+          id: true,
+          name: true,
+          email: true,
+          createdAt: true,
+          checkIn: {
+            select: {
+              createdAt: true,
+            },
+          },
+        },
+      });
+
+      const totalAttendeesCountPromise = prisma.attendee.count({
+        where: query
+          ? {
+              eventId,
+              name: {
+                contains: query,
+              },
+            }
+          : {
+              eventId,
+            },
+        take: 10,
+        skip: pageIndex * 10,
+        orderBy: {
+          createdAt: "desc",
+        },
+      });
+
+      const [attendees, total] = await Promise.all([
+        attendeesPromise,
+        totalAttendeesCountPromise,
+      ]);
+
+      return reply.send({
+        attendees: attendees.map((attendee) => {
+          return {
+            id: attendee.id,
+            name: attendee.name,
+            email: attendee.email,
+            createdAt: attendee.createdAt,
+            checkedInAt: attendee.checkIn?.createdAt ?? null,
+          };
+        }),
+        total,
+      });
+    },
+  );
+}
