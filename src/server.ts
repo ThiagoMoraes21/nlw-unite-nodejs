8a8845051dd22cf078778f298df54b19f8e83ce1
ThiagoMoraes21 <thiago4x70@gmail.com>
2024-04-07 15:59:47 -0300

    feat: add route check-in

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,7 @@ import {
 import { createEvent } from "./routes/create-events";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
+import { checkIn } from "./routes/check-in";
 
 const app = fastify();
 
@@ -21,6 +22,7 @@ app.get("/health-check", () => {
 app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
+app.register(checkIn);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("http server is running...");
