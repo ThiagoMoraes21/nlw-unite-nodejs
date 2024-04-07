e446f76a9fe3de45270d17182cd3bef759bf4c59
ThiagoMoraes21 <thiago4x70@gmail.com>
2024-04-07 11:18:49 -0300

    feat: create route /events/:eventId/attendees, for register a new event attendee

--- src/routes/register-for-event.ts
+++ src/routes/register-for-event.ts
@@ -4,37 +4,82 @@ import z from "zod";
 import { prisma } from "../lib/prisma";
 
 export async function registerForEvent(app: FastifyInstance) {
-    app
-        .withTypeProvider<ZodTypeProvider>()
-        .post("/events/:eventId/attendees",
-            {
-                schema: {
-                    params: z.object({
-                        eventId: z.string().uuid()
-                    }),
-                    body: z.object({
-                        name: z.string().min(4),
-                        email: z.string().email()
-                    }),
-                    respose: {
-                        201: z.object({
-                            attendeeId: z.number(),
-                        })
-                    }
-                }
-            },
-            async (request, reply) => {
-                const { eventId } = request.params;
-                const { name, email } = request.body;
+  app.withTypeProvider<ZodTypeProvider>().post(
+    "/events/:eventId/attendees",
+    {
+      schema: {
+        params: z.object({
+          eventId: z.string().uuid(),
+        }),
+        body: z.object({
+          name: z.string().min(4),
+          email: z.string().email(),
+        }),
+        respose: {
+          201: z.object({
+            attendeeId: z.number(),
+          }),
+        },
+      },
+    },
+    async (request, reply) => {
+      const { eventId } = request.params;
+      const { name, email } = request.body;
 
-                const attendee = await prisma.attendee.create({
-                    data: {
-                        name,
-                        email,
-                        eventId
-                    }
-                });
+      await validateMaximumEventAttendees(eventId);
+      await validateAttendeeUniqueness(eventId, email);
 
-                return reply.status(201).send({ attendeeId: attendee.id });
-            });
+      const attendee = await prisma.attendee.create({
+        data: {
+          name,
+          email,
+          eventId,
+        },
+      });
+
+      return reply.status(201).send({ attendeeId: attendee.id });
+    },
+  );
 }
+
+async function validateMaximumEventAttendees(eventId: string) {
+  const eventPromise = prisma.event.findUnique({
+    where: {
+      id: eventId,
+    },
+  });
+
+  const eventAttendeesCountPromise = prisma.attendee.count({
+    where: {
+      eventId,
+    },
+  });
+
+  const [event, eventAttendeesCount] = await Promise.all([
+    eventPromise,
+    eventAttendeesCountPromise,
+  ]);
+
+  if (
+    event?.maximumAttendees &&
+    eventAttendeesCount >= event.maximumAttendees
+  ) {
+    throw new Error("Maximum attendees limit was exceeded!");
+  }
+}
+
+async function validateAttendeeUniqueness(eventId: string, email: string) {
+  const attendeeFromEmail = await prisma.attendee.findUnique({
+    where: {
+      eventId_email: {
+        email,
+        eventId,
+      },
+    },
+  });
+
+  if (attendeeFromEmail !== null) {
+    throw new Error("This e-mail is already registered for this event!");
+  }
+}
+
