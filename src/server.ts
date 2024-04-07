36f0b27640910c94b6ddf3ef1da8787b9d7ae826
ThiagoMoraes21 <thiago4x70@gmail.com>
2024-04-07 20:24:21 -0300

    feat: add cors configuration, set error handler and configure swagger

--- src/server.ts
+++ src/server.ts
@@ -1,31 +1,61 @@
-import fastify from "fastify";
 import {
   serializerCompiler,
   validatorCompiler,
   ZodTypeProvider,
+  jsonSchemaTransform,
 } from "fastify-type-provider-zod";
 import { createEvent } from "./routes/create-events";
 import { registerForEvent } from "./routes/register-for-event";
 import { getEvent } from "./routes/get-event";
 import { checkIn } from "./routes/check-in";
 import { getEventAttendees } from "./routes/get-event-attendees";
+import { getAttendeeBadge } from "./routes/get-attendee-badge";
+import { errorHandler } from "./error-handler";
 
-const app = fastify();
+import fastify from "fastify";
+
+import fastifySwagger from "@fastify/swagger";
+import fastifySwaggerUI from "@fastify/swagger-ui";
+import fastifyCors from "@fastify/cors";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
+
+app.register(fastifySwagger, {
+  swagger: {
+    consumes: ["application/json"],
+    produces: ["application/json"],
+    info: {
+      title: "pass.in",
+      description:
+        "Especificações da API para o back-end da aplicação pass.in construída durante o NLW Unite da Rocketseat.",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(fastifySwaggerUI, {
+  routePrefix: "/docs",
+});
 
 // Add schema validator and serializer
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
 
-app.get("/health-check", () => {
-  return "hello form terminal";
-});
-
+// Add routes
 app.register(createEvent);
 app.register(registerForEvent);
 app.register(getEvent);
 app.register(checkIn);
 app.register(getEventAttendees);
+app.register(getAttendeeBadge);
 
-app.listen({ port: 3333 }).then(() => {
+app.setErrorHandler(errorHandler);
+
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
   console.log("http server is running...");
 });
